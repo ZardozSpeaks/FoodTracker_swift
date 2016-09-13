131d9969331cafd908b7c50f339155820640dffd
David Remington <david.ian.remington@gmail.com>
2016-09-12 18:25:23 -0700

    add logic for rating control feature

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,6 +15,8 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
     
 
     //MARK: Initialization
@@ -23,14 +25,28 @@ class RatingControl: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.redColor()
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
-        addSubview(button)
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
+            button.frame = buttonFrame
+        }
     }
     
     override func intrinsicContentSize() -> CGSize {
-        return CGSize(width: 240, height: 44)
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        return CGSize(width: width, height: buttonSize)
     }
     
     //MARK: Button Action
